75c7b46c3e669bdf70fa41dfd75fdfe0c9e778bf
inlve_iza <202312261@gordoncollege.edu.ph>
2025-12-11 19:33:33 +0800

    Updated system files

--- my-app/src/app/context/CategoryContext.tsx
+++ my-app/src/app/context/CategoryContext.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { createContext, useContext, useState, ReactNode } from 'react';
+import { createContext, ReactNode, useContext, useState } from 'react';
 
 interface CategoryContextType {
   selectedCategory: string;
